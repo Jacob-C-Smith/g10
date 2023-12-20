2d67a6ef6e61fab23697edb9179a8108b65141c5
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-19 19:30:21 -0700

    Updated example program

--- main.c
+++ main.c
@@ -7,11 +7,18 @@
 
 // Standard library
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+// http module
+#include <http/http.h>
+
+// web module
+#include <web/web.h>
+#include <web/route.h>
 
 // g10
 #include <g10/g10.h>
-
-// Callback registration
 #include <g10/user_code.h>
 
 /** !
@@ -31,9 +38,20 @@ int user_code_main ( g_instance *const p_instance )
     return 1;
 }
 
+int index_router ( http_message *p_request, http_message *p_response )
+{
+
+    // Success
+    return 1;
+}
+
 // Entry point
 int main ( int argc, const char *const argv[] )
 {
+
+    // Supress compiler warnings
+    (void) argc;
+    (void) argv;
 
     // Initialized data
     g_instance *p_instance = 0;
@@ -47,6 +65,21 @@ int main ( int argc, const char *const argv[] )
     // Call the user code
     user_code_callback(p_instance);
 
+    // Start the server
+    g_start_server(p_instance);
+
+    // Set the running flag
+    p_instance->running = true;
+
+    // Main loop
+    while (p_instance->running)
+    {
+        sleep(1);
+        //printf("%d        \r", z);
+        //z++;
+
+    }
+    
     // Clean up g10
     if ( g_exit(&p_instance) == 0 ) goto failed_to_teardown_g10;
 
