9fa47ae0f16c74bcd49c507b8c9c1229a13b64dc
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-06-18 20:01:34 -0600

    [PLY] Started PLY parser

---
+++ include/g10/ply.h
@@ -0,0 +1,23 @@
+/** !
+ * Polygon file format
+ * 
+ * @file g10/ply.h
+ * 
+ * @author Jacob Smith
+ */
+
+// Include guard
+#pragma once
+
+// Standard library
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// hash cache module
+#include <hash_cache/hash.h>
+
+// g10
+#include <g10/gtypedef.h>
+
