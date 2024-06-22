3a75a5e8a3f83b8d4c068ff046caddf75246047a
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-06-22 13:10:02 -0600

    [sdl2] Improved error handling

--- 3rd-party/g_sdl2.c
+++ 3rd-party/g_sdl2.c
@@ -6,8 +6,9 @@ int g_sdl2_initialize_window ( g_instance *p_instance, json_value *p_value )
 {
 
     // Argument check
-    if ( p_instance == (void *) 0 ) goto no_instance;
-    if ( p_value    == (void *) 0 ) goto no_value;
+    if ( p_instance    ==        (void *) 0 ) goto no_instance;
+    if ( p_value       ==        (void *) 0 ) goto no_value;
+    if ( p_value->type != JSON_VALUE_OBJECT ) goto wrong_type;
 
     // Initialized data
     u32 sdl2_window_flags = SDL_WINDOW_SHOWN;
@@ -19,9 +20,6 @@ int g_sdl2_initialize_window ( g_instance *p_instance, json_value *p_value )
         sdl2_window_flags |= SDL_WINDOW_OPENGL;
     #endif
 
-    // Parse the window json
-    if ( p_value->type != JSON_VALUE_OBJECT ) goto wrong_type;
-
     // Parse the window object
     {
 
@@ -32,9 +30,11 @@ int g_sdl2_initialize_window ( g_instance *p_instance, json_value *p_value )
                    *p_title  = (json_value *)dict_get(p_dict, "title");
 
         // Error check
-        if ( ! ( p_width && p_height && p_title ) ) goto missing_properties;
+        if ( p_width  == (void *) 0 ) goto no_width_property;
+        if ( p_height == (void *) 0 ) goto no_height_property;
+        if ( p_title  == (void *) 0 ) goto no_title_property;
 
-        // Error check
+        // Type check
         if ( p_width->type  != JSON_VALUE_INTEGER ) goto wrong_width_type;
         if ( p_height->type != JSON_VALUE_INTEGER ) goto wrong_height_type;
         if ( p_title->type  != JSON_VALUE_STRING  ) goto wrong_title_type;
@@ -71,31 +71,139 @@ int g_sdl2_initialize_window ( g_instance *p_instance, json_value *p_value )
     // Success
     return 1;
 
-    no_instance:
-    no_value:
-    wrong_type:
-    missing_properties:
-    wrong_width_type:
-    wrong_height_type:
-    wrong_title_type:
-    no_mem:
-    
-        // Error
-        return 0;
+    // Error handling
+    {
+
+        // Argument errors
+        {
+            no_instance:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Null pointer provided for parameter \"p_instance\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+
+            no_value:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Null pointer provided for parameter \"p_value\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+
+            wrong_type:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Parameter \"p_value\" must be of type [ object ] in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+
+        // json errors
+        {
+            no_width_property:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Parameter \"p_value\" is missing required property \"width\" in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+
+            no_height_property:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Parameter \"p_value\" is missing required property \"height\" in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+
+            no_title_property:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Parameter \"p_value\" is missing required property \"title\" in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+
+            wrong_width_type:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Property \"width\" of parameter \"p_value\" must be of type [ integer ] in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+
+            wrong_height_type:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Property \"height\" of parameter \"p_value\" must be of type [ integer ] in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+
+            wrong_title_type:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Property \"title\" of parameter \"p_value\" must be of type [ string ] in call to function \"%s\"\n", __FUNCTION__);
+                    log_info("\tRefer to gschema: https://schema.g10.app/instance.json\n");
+                #endif
+
+                // Error
+                return 0;
+        }
+
+        // Standard library errors
+        {
+            no_mem:
+                #ifndef NDEBUG
+                    log_error("[Standard Library] Failed to allocate memory in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+    }
 }
 
 int g_sdl2_destroy_window ( g_instance *p_instance )
 {
+
+    // Argument check
+    if ( p_instance == (void *) 0 ) goto no_instance;
 
     // Destroy the window
     SDL_DestroyWindow(p_instance->window.sdl2.window);
 
     // Success
     return 1;
+
+    // Error handling
+    {
+
+        // Argument errors
+        {
+            no_instance:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Null pointer provided for parameter \"p_instance\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+    }
 }
 
 int g_sdl2_poll_window ( g_instance *p_instance )
 {
+
+    // Argument check
+    if ( p_instance == (void *) 0 ) goto no_instance;
 
     // Process each event
     while (SDL_PollEvent(&p_instance->window.sdl2.event))
@@ -125,4 +233,19 @@ int g_sdl2_poll_window ( g_instance *p_instance )
 
     // Success
     return 1;
+
+    // Error handling
+    {
+
+        // Argument errors
+        {
+            no_instance:
+                #ifndef NDEBUG
+                    log_error("[g10] [sdl2] Null pointer provided for parameter \"p_instance\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+    }
 }
