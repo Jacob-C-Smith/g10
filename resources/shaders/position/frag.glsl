b4ed22670374dacb83ae6c42dae2b3362a7dcdfb
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-07-15 17:13:17 -0600

    [resources] Added position shader

---
+++ resources/shaders/position/frag.glsl
@@ -0,0 +1,22 @@
+/** !
+ * @file resources/shaders/solid_color/frag.glsl
+ *
+ * @author Jacob Smith
+ */
+
+// 4.6
+#version 460 core
+
+// Input
+in vec3 xyz;
+
+// Output
+out vec4 FragColor;
+
+// Entry point
+void main()
+{
+
+   // Store the color in the framebuffer
+   FragColor = vec4(vec3(xyz), 1.0f);
+}
